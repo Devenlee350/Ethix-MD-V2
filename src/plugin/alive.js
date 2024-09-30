4e79062b729290f2ec03d48028b2f874062f16c0
STRIKERBOY <110353280+Devenlee350@users.noreply.github.com>
2024-09-30 18:34:08 +0200

    𝚂𝚃𝚁𝙸𝙺𝙴𝚁𝙱𝙾𝚈

--- src/plugin/alive.js
+++ src/plugin/alive.js
@@ -26,7 +26,7 @@ const alive = async (m, Matrix) => {
     image.print(font, x, y, timeString, width, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
     const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
     
-    const uptimeMessage = `*🤖 ETHIX-MD Status Overview*
+    const uptimeMessage = `*🤖 𝙽𝙾𝚅𝙰 Status Overview*
 _________________________________________
 
 *📆 ${days} Day(s)*
